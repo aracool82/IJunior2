480a2f68f23b13988c948d4506f3457a2f9d4ae5
Bulaev Alexey <aracool1982@gmail.com>
2020-05-29 16:08:16 +0700

    Добавлен скрипт CameraFallow.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+
+    void Update()
+    {
+        transform.position = GetPosition();
+    }
+
+    private Vector3 GetPosition()
+    {
+        return new Vector3(_player.transform.position.x, transform.position.y, transform.position.z);
+    }
+}
