0bc88e59dd3c716182ea28f14b775117cf324134
Bulaev Alexey <aracool1982@gmail.com>
2020-06-08 23:19:35 +0700

    создано меню взамен 3х мелких иенюшек

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        Time.timeScale = 0;
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
