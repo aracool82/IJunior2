50c85a236f211a17d1bf9a75adb0f10773be0a3a
Bulaev Alexey <aracool1982@gmail.com>
2020-06-08 22:03:02 +0700

    изменен CameraFollow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,15 +2,15 @@
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private Transform _target;
 
-    void Update()
+    private void Update()
     {
         transform.position = GetPosition();
     }
 
     private Vector3 GetPosition()
     {
-        return new Vector3(_playerTransform.position.x, transform.position.y, transform.position.z);
+        return new Vector3(_target.position.x, transform.position.y, transform.position.z);
     }
 }
